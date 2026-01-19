5e4581309d9286f7b56558a214dd7dab9ae1dfac
zinwaithin <zinwaithin06@gmail.com>
2026-01-19 22:33:51 +0630

    origin

---
+++ JSlecture/JS/datatype.js
@@ -0,0 +1,48 @@
+//string data type
+let name = "Z W T"
+console.log(name);
+console.log(typeof(name));
+
+//numeric data type
+let age = 20
+console.log (age);
+console.log (typeof(age));
+
+//boolean data type
+let single=true;
+console.log(single);
+console.log(typeof(single));
+
+//undifined
+let myBaby;
+console.log(myBaby);
+
+//null
+let son =null;
+son = "lwin";
+console.log(son);
+console.log(typeof(son));
+
+//array
+let arr1 = new Array('a','b','c')
+console.log(arr1);
+console.log(typeof(arr1));
+
+let arr2 = [1,2,3,4,5]
+console.log(arr2);
+console.log(typeof(arr2));
+
+//multiarry
+let multi = ['Zin Zin','Wai Wai',['Aung Aung','Mung Mung'],'Thin Thin']
+console.log(multi);
+console.log(typeof(multi));
+
+//object
+let student ={
+    //Key:value
+    name:'Maung Maung',
+    age:20,
+    address:'Yangon',
+}
+console.log(student);
+console.log(typeof(student));
